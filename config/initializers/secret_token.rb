bde56fe29b913da8943e0ecada99ff061e4b2003
Petter Gjöres <gjores@Petters-MacBook-Pro.local>
2013-09-24 21:20:43 +0200

    first commit with User model, devise rolify and cancan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ibookbackend::Application.config.secret_token = 'd17f35ffa4dfa112fa590d6826fafa5654cd54ff838d6f0b1b5bba81c50c21fa2a2d37062a0c2ae4ba638e1d12af2027c1fca17bd51ef786616501b56ea3e04e'
